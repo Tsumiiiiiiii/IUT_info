ca8a41723b113ed23a5d1a06cbff96636209e5f7
sumitalamkhan <31077557+sumitalamkhan@users.noreply.github.com>
2020-01-01 18:34:22 +0600

    Update process.py

--- process.py
+++ process.py
@@ -1,5 +1,16 @@
 import os
 
+def refined_name(name):
+    name=name.split(' ')
+    r_name=''
+    for c in name:
+        if c!=' ':
+            r_name+=c
+    return r_name
+
+def check_name(name1,name2):
+    return refined_name(name1)==refined_name(name2)
+
 def findSubjectAndScholarship(name):
     dir = os.path.dirname(__file__)
     path = os.path.join(dir, 'information.txt')
@@ -10,7 +21,7 @@ def findSubjectAndScholarship(name):
     scholarship=0
     for line in f:
         info=line.split(',')
-        if name==str(info[0])[:-1]:
+        if check_name(name,str(info[0])[:-1]):
             response=1
             subject=info[1]
             scholarship=info[2]
@@ -26,11 +37,9 @@ def findPosition(name,subject):
     f=p.readlines()
     for line in f:
         info=line.split(',')
-        #print(info[1])
         if subject in info[1]:
-            #print(subject)
             position+=1
-        if name==str(info[0])[:-1]:
+        if check_name(name,str(info[0])[:-1]):
             break
     p.close()
     return position
